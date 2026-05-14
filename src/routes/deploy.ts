88eb02d3bcea42c6b18ad9eaaa19a5241e42167b
vivek1504 <vivekjadhav2743@gmail.com>
2026-05-14 15:52:55 +0530

    replaced sync exec with async exec

--- src/routes/deploy.ts
+++ src/routes/deploy.ts
@@ -4,9 +4,13 @@ import crypto from "crypto";
 import path from "path";
 import extract from "extract-zip";
 import fs from "fs";
-import { execSync, spawn } from "child_process";
+import { exec as execCb, spawn } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(execCb);
 import axios from "axios";
 import { getPaths } from "../utils/path.js";
+import { performance } from "perf_hooks";
 
 export const deployRouter = Router();
 
@@ -52,21 +56,33 @@ async function deployFunction(zipPath: string) {
   const paths = getPaths(functionId);
 
   try {
+    const t0 = performance.now();
     await extractZip(zipPath, paths.outputDir);
+    console.log("extract:", performance.now() - t0);
     await fs.promises.unlink(zipPath);
 
-    const image = prepareRootfs(functionId);
+    const t1 = performance.now();
+    const image = await prepareRootfs(functionId);
+    console.log("rootfs:", performance.now() - t1);
 
+    const t2 = performance.now();
     const fc = await startFirecrackerProcess(paths.apiSock);
+    console.log("fc spawn:", performance.now() - t2);
 
+    const t3 = performance.now();
     const readyPromise = waitForVMReady(fc);
-
     const client = createFcCient(paths.apiSock);
 
+    const t4 = performance.now();
     await configureVM(client, functionId, image);
+    console.log("configure Vm: ", performance.now() - t4);
 
     await readyPromise;
+    console.log("wait for vmReady: ", performance.now() - t3);
+
+    const t5 = performance.now();
     await snapshotVM(client, functionId);
+    console.log("snapshot time: ", performance.now() - t5);
 
     fc.kill("SIGKILL");
     return {
@@ -74,7 +90,9 @@ async function deployFunction(zipPath: string) {
       url: `http://localhost:3000/f/${functionId}`,
     };
   } finally {
+    const t6 = performance.now();
     await cleanupResources(paths);
+    console.log("cleanupResources: ", performance.now() - t6);
   }
 }
 
@@ -89,22 +107,22 @@ async function extractZip(zip: string, outputDir: string) {
   });
 }
 
-function prepareRootfs(functionId: string) {
+async function prepareRootfs(functionId: string) {
   const baseImage = "rootfs.ext4";
   const image = `rootfs/rootfs-${functionId}.ext4`;
 
-  execSync(`cp ${baseImage} ${image}`);
+  await exec(`cp --reflink=auto ${baseImage} ${image}`);
 
   const mountDir = `/mnt/rootfs-${functionId}`;
   const extracted = `extracted/${functionId}`;
 
-  execSync(`sudo mkdir -p ${mountDir}`);
-  execSync(`sudo mount -o loop ${image} ${mountDir}`);
+  await exec(`sudo mkdir -p ${mountDir}`);
+  await exec(`sudo mount -o loop ${image} ${mountDir}`);
 
-  execSync(`sudo cp -r ${extracted}/. ${mountDir}/app/`);
+  await exec(`sudo cp -r ${extracted}/. ${mountDir}/app/`);
 
-  execSync(`sudo umount ${mountDir}`);
-  execSync(`sudo rm -rf ${mountDir}`);
+  await exec(`sudo umount ${mountDir}`);
+  await exec(`sudo rm -rf ${mountDir}`);
 
   return image;
 }
